523af48f3f2faef8a90c9fdeab0dfd7d039188e7
Md Rafi Akhtar <alimdrafi@gmail.com>
2022-05-03 19:50:23 +0530

    Done with Data flow of reactive forms in the intro

--- learning-notes.md
+++ learning-notes.md
@@ -32,3 +32,12 @@ Both reactive and template-driven forms are build on the following classes.
 3. `FormArray`
 4. `ControlValueAccessor`
 
+## Data Flow in Reactive Forms
+Both the following images are taken from Angular's website.
+1. View to value ![Data Flow in Reactive Forms: View to Value](https://angular.io/generated/images/guide/forms-overview/dataflow-reactive-forms-vtm.png)
+2. Value to vieww ![Data Flow in Reactive Forms: Value to View](https://angular.io/generated/images/guide/forms-overview/dataflow-reactive-forms-mtv.png)
+
+### Mutability of the Data Model
+With reactive Forms, the `FormControl` instance always returns a new object value when the control's value is updated (more efficient).
+This is in comparison with template forms, which updates the form property to its new value (less efficient).
+
--- src/app/favorite-color/favorite-color.component.ts
+++ src/app/favorite-color/favorite-color.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -6,13 +6,18 @@ import { FormControl } from '@angular/forms';
   templateUrl: './favorite-color.component.html',
   styleUrls: ['./favorite-color.component.scss']
 })
-export class FavoriteColorComponent implements OnInit {
+export class FavoriteColorComponent implements OnInit, OnChanges {
   public favoriteColorControl: FormControl;
 
   constructor() { }
 
   ngOnInit(): void {
     this.favoriteColorControl = new FormControl('');
+    // this.favoriteColorControl.setValue('Red');
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log('form value:', this.favoriteColorControl.value);
   }
 
 }
